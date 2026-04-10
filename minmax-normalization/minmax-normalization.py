21e370012f8814910a4ccc1b137396077f91fdf8
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-04-10 15:12:40 +0200

    Add solution for minmax-normalization

---
+++ minmax-normalization/minmax-normalization.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def minmax_scale(X, axis=0, eps=1e-12):
+    """
+    Scale X to [0,1]. If 2D and axis=0 (default), scale per column.
+    Return np.ndarray (float).
+    """
+    # Write code here
+    mini = np.min(X,axis, keepdims = True)
+    maxi = np.max(X,axis, keepdims = True)
+
+    deno = np.maximum(maxi-mini, eps)
+
+    return (X-mini)/deno
