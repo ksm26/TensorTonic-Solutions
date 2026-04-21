da436efffc7fac8fb7efc50f1b923c7dc4b9b36c
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-04-21 09:32:22 +0200

    Update solution for interaction-features

--- interaction-features/interaction-features.py
+++ interaction-features/interaction-features.py
@@ -2,16 +2,15 @@ def interaction_features(X):
     """
     Generate pairwise interaction features and append them to the original features.
     """
-    # Write code here
     result = []
-    
-    for row in X:
-        new_row = row[:]
+    for i in range(len(X)):
+        row = X[i]
+        out = []
+        for j in range(len(row)):
+            for k in range(j+1,len(row)):
+                out.append(row[k]*row[j])
 
-        for i in range(len(row)):
-            for j in range(i+1, len(row)):
-                new_row.append(row[i] * row[j])
-    
-        result.append(new_row)
+        result.append(row+out)
 
     return result
+            
