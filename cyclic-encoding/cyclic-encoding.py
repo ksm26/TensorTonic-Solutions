c65c0d8d7c7dbaf2782afded8ec4c62d5998e1a7
Khushdeep-singh <khushdeepvnit@gmail.com>
2026-04-21 09:41:01 +0200

    Update solution for cyclic-encoding

--- cyclic-encoding/cyclic-encoding.py
+++ cyclic-encoding/cyclic-encoding.py
@@ -2,12 +2,9 @@ def cyclic_encoding(values, period):
     """
     Encode cyclic features as sin/cos pairs.
     """
-    # Write code here
     result = []
-
-    for v in values :
-        theta = 2*math.pi * v/period
-        
+    for v in values:
+        theta = 2*math.pi*v/period
         result.append([math.sin(theta), math.cos(theta)])
 
     return result
